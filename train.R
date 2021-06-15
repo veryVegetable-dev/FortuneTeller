0fa23ed520c8ea1d95b6c339f8f481f77a31b5c6
nomad <mengling_wu@icloud.com>
2021-06-15 22:29:01 +0800

    train.R

--- train.R
+++ train.R
@@ -15,16 +15,5 @@ duration=log$duration
 rpart_param = rpart.control(minsplit = 1, minbucket = 1)
 cfit <- rpart(y ~ period + sleeping + resting_heart_rate + task + duration, data = log, method = 'class', control = rpart_param)
 saveRDS(cfit, "teller.model")
-my_model = readRDS("teller.model")
-my_model
-
-# predict on new data 
-test_data = log[nrow(log), ]
-data.frame(predict(my_model, newdata=test_data, type="prob"))[, 2]
-
-
-a = strsplit("1 1 1", split = " ")
-df = data.frame(c(1, 2, 3))
-
 
 
